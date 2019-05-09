963f003b500d15986019d0046fac287c75a9dbc2
Roman Batsenko <rbatsenko@gmail.com>
2019-05-09 12:23:11 +0200

    init commit

---
+++ CarSalesman.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class CarSalesman {
+  private string _firstName;
+  private string _lastName;
+
+  public string FullName {
+    get {
+      return string.Format("{0} {1}", this._firstName, this._lastName);
+    }
+  }
+
+  public CarSalesman( string firstName, string lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  public void Sell() {
+    Console.WriteLine(string.Format("Hi, my name is {0}. I would recommend You to by this car.", this.FullName));
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarSalesman steveTheSalesman = new CarSalesman("Steve", "Rogers");
+            Console.WriteLine(steveTheSalesman.FullName);
+            steveTheSalesman.Sell();
+
+            CarSalesman erik = new CarSalesman("Erik", "Erikson");
+            Console.WriteLine(erik.FullName);
+            erik.Sell();
+        }
+    }
+}
