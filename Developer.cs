2f3c23254cd46aee3ae9c18da5da007d09fe230a
Roman Batsenko <rbatsenko@gmail.com>
2019-05-09 15:27:08 +0200

    static polymorphism

--- Developer.cs
+++ Developer.cs
@@ -11,6 +11,16 @@ public abstract class Developer
 
   public virtual void Code()
   {
-    Console.WriteLine(String.Format("I code in {0}", this._mainDevLanguage));
+    Console.WriteLine(String.Format("I code in {0}.", this._mainDevLanguage));
+  }
+
+  public virtual void Code(string firstExtraDevLanguage)
+  {
+    Console.WriteLine(String.Format("I code in {0} and I also use {1}.", this._mainDevLanguage, firstExtraDevLanguage));
+  }
+
+  public virtual void Code(string firstExtraDevLanguage, string secondExtraDevLanguage)
+  {
+    Console.WriteLine(String.Format("I code in {0} and I also use {1} and {2}.", this._mainDevLanguage, firstExtraDevLanguage, secondExtraDevLanguage));
   }
 }
